873638af7bc41481187e34b7e3d26be8c99530b3
Matthew Cook <cooker8200@hotmail.com>
2023-12-15 22:34:16 -0600

    first test

--- test/Main.test.tsx
+++ test/Main.test.tsx
@@ -5,8 +5,6 @@ import Main from '../src/scripts/Main';
 
 test('it should render the page title', () => {
   render(<Main />);
-  const title = screen.getByTestId('main__title');
-  console.log(title);
 
   expect(screen.getByTestId('main__title')).toHaveTextContent('Date Calculator');
 });
